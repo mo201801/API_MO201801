9336c26641fb50d152eb991cc50aabb0f5d2a2ba
Rafael Marroquin <Rafael Marroquin@LAPTOP-9MNFPFSA>
2023-09-28 21:38:01 -0600

    Agregar archivos de proyecto.

---
+++ API_MO201801/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="API_MO201801.WebApiApplication" Language="C#" %>
---
+++ API_MO201801/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_MO201801.Models
+{
+    public class Employee
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime JoiningDate { get; set; }   
+        public int Age { get; set; }    
+        
+
+
+
+    }
+}
